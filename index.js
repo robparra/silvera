43cbe5f913b56c524d3a1816bb47bc2bd8d80c87
Robert <parralrob@gmail.com>
2021-07-20 14:06:38 -0400

    prueba silvera

---
+++ index.js
@@ -0,0 +1,15 @@
+var getData= function(){
+    var email = document.getElementById("email").value;
+    console.log(email)
+    alert("Mensaje enviado")
+}
+
+var getData2= function(){
+    var nombre = document.getElementById("nombre").value;
+    var fecha = document.getElementById("fecha").value;
+    var servicio = document.getElementById("servicio").value;
+    var mensaje = document.getElementById("mensaje").value;
+    var email2 = document.getElementById("email2").value;
+    console.log(nombre+" "+fecha+" "+servicio+" "+mensaje+" "+email2)
+    alert("Mensaje enviado")
+}
